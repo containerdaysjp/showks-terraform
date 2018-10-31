080e21881a57dc5794771f7a21e3c919a74be163
Kazuto Kusama <jacopen@gmail.com>
2018-10-31 17:12:01 +0900

    Add staging cluster

--- gke.tf
+++ gke.tf
@@ -1,15 +1,35 @@
-resource "google_container_cluster" "showks-cluster" {
-    name               = "${var.cluster_name}"
+resource "google_container_cluster" "showks-cluster-prod" {
+    name               = "${var.cluster_name}-prod"
     zone               = "${var.zone}"
     initial_node_count = "${var.initial_node_count}"
 
     network            = "${var.network}"
 
-    min_master_version     = "${var.min_master_version}"
+    min_master_version = "${var.min_master_version}"
     node_version       = "${var.node_version}"
 
     node_config {
         machine_type   = "${var.machine_type}"
+    }
+    master_auth {
+        username       = "${var.master_auth_username}"
+        password       = "${var.master_auth_password}"
+    }
+}
+
+resource "google_container_cluster" "showks-cluster-stg" {
+    name               = "${var.cluster_name}-stg"
+    zone               = "${var.zone}"
+    initial_node_count = "${var.initial_node_count}"
+
+    network            = "${var.network}"
+
+    min_master_version = "${var.min_master_version}"
+    node_version       = "${var.node_version}"
+
+    node_config {
+        machine_type   = "${var.machine_type}"
+        preemptible    = true
     }
     master_auth {
         username       = "${var.master_auth_username}"
