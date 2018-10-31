f609cf9beae1aa02670d7792a0dec44c117fd1b6
Kazuto Kusama <jacopen@gmail.com>
2018-11-01 01:43:44 +0900

    Add static ips

--- dns.tf
+++ dns.tf
@@ -11,3 +11,33 @@ resource "google_dns_record_set" "jumpbox" {
 
   rrdatas = ["${google_compute_address.jumpbox-ip.address}"]
 }
+
+resource "google_dns_record_set" "concourse" {
+  name = "concourse.${data.google_dns_managed_zone.showks_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.showks_zone.name}"
+
+  rrdatas = ["${google_compute_global_address.concourse_ip.address}"]
+}
+
+resource "google_dns_record_set" "spinnaker_api" {
+  name = "spinnaker-api.${data.google_dns_managed_zone.showks_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.showks_zone.name}"
+
+  rrdatas = ["${google_compute_global_address.spinnaker_api_ip.address}"]
+}
+
+resource "google_dns_record_set" "spinnaker_ui" {
+  name = "spinnaker-ui.${data.google_dns_managed_zone.showks_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.showks_zone.name}"
+
+  rrdatas = ["${google_compute_global_address.spinnaker_ui_ip.address}"]
+}
--- static_ip.tf
+++ static_ip.tf
@@ -2,3 +2,15 @@ resource "google_compute_address" "jumpbox-ip" {
   name = "${var.env_name}-jumpbox-ip"
 }
 
+resource "google_compute_global_address" "concourse_ip" {
+  name = "${var.env_name}-concourse-ip"
+}
+
+resource "google_compute_global_address" "spinnaker_api_ip" {
+  name = "${var.env_name}-api-ip"
+}
+
+resource "google_compute_global_address" "spinnaker_ui_ip" {
+  name = "${var.env_name}-ui-ip"
+}
+
