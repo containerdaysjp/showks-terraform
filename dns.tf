1b4e8433124068dc3706d7cfd3817955eb73ba82
Kazuto Kusama <jacopen@gmail.com>
2018-11-24 00:07:34 +0900

    Modify DNS

--- dns.tf
+++ dns.tf
@@ -61,3 +61,34 @@ resource "google_dns_record_set" "stg_wildcard" {
 
   rrdatas = ["${google_dns_record_set.stg_ingress_nginx.name}"]
 }
+
+resource "google_dns_record_set" "github_pages" {
+  name = "${data.google_dns_managed_zone.showks_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.showks_zone.name}"
+
+  rrdatas = ["185.199.108.153","185.199.109.153","185.199.110.153","185.199.111.153"]
+}
+
+resource "google_dns_record_set" "prod_ingress_nginx" {
+  name = "ingress-nginx.prod.${data.google_dns_managed_zone.showks_zone.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.showks_zone.name}"
+
+  rrdatas = ["${var.prod_ingress_nginx_address}"]
+}
+
+resource "google_dns_record_set" "prod_wildcard" {
+  name = "*.prod.${data.google_dns_managed_zone.showks_zone.dns_name}"
+  type = "CNAME"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.showks_zone.name}"
+
+  rrdatas = ["${google_dns_record_set.prod_ingress_nginx.name}"]
+}
+
