1ce93789a37ed8f08812072ecbf37ab27d2e2c4b
Kazuto Kusama <jacopen@gmail.com>
2018-12-04 03:36:34 +0900

    Fix minor bug

--- gcs.tf
+++ gcs.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "prod_concourse_store" {
   name     = "prod-${var.env_name}-concourse-bucket"
   location = "${var.region}"
-  storage_class = "regional"
+  storage_class = "REGIONAL"
 
   website {
     main_page_suffix = "index.html"
@@ -11,7 +11,7 @@ resource "google_storage_bucket" "prod_concourse_store" {
 resource "google_storage_bucket" "stg_concourse_store" {
   name     = "stg-${var.env_name}-concourse-bucket"
   location = "${var.region}"
-  storage_class = "regional"
+  storage_class = "REGIONAL"
 
   website {
     main_page_suffix = "index.html"
